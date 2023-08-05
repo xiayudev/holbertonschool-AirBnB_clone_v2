d44bfe5cf155b4f2f56a6cff9f884922890c7c49
xiayudev <jctuesta7@gmail.com>
2023-08-05 13:39:07 -0700

    fix: position of arguments in Place

--- models/place.py
+++ models/place.py
@@ -13,12 +13,12 @@ if os.getenv("HBNB_TYPE_STORAGE") == "db":
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True, default=0)
-        longitude = Column(Float, nullable=True, default=0)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, default=0, nullable=True)
+        longitude = Column(Float, default=0, nullable=True)
 
 else:
     class Place(BaseModel):
