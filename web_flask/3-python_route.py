42ba2f9ff227bb15fc703f56e857c1943f901e13
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-14 11:49:45 -0500

    adding a script that starts a Flask web application with other new method

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def task0():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def task1():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def task2(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def task3(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
