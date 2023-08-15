69d0726de81114681ab037552b349ae000db0382
xiayudev <jctuesta7@gmail.com>
2023-08-15 16:31:29 -0500

    feat: create a basic web app for aribnb clone

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-from flask import Flask
+"""Module to start a Flask web application"""
 
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def task0():
+@app.route("/", strict_slashes=False)
+def index():
     return "Hello HBNB!"
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
