a24f07542be09540445fe69aa5c0c5d152f76400
xiayudev <jctuesta7@gmail.com>
2023-08-15 16:38:30 -0500

    fix: insert a readme for presenting the project

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
+    """Index page"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
---
+++ web_flask/README.md
@@ -0,0 +1,5 @@
+# Flask Web Applications
+
+In this project we will cover:
++ Create a web app with Flask
++ Use jinja2 for templates
