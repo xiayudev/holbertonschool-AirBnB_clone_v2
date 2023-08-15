d1e4ff6ad62c510942547b6971688bb5c7c0f445
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-14 22:35:14 -0700

    fix: change close() to remove()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -61,4 +61,4 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute
         (self.__session)"""
-        self.__session.close()
+        self.__session.remove()
